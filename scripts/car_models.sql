ed830a3499d1bce49743c7338f47d82a97750efd
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-31 13:47:37 -1000

    Finished assignment.

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,8 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX make_idx ON car_models (make_code);
+CREATE INDEX model_idx ON car_models (model_code);
+CREATE INDEX year_idx ON car_models (year);
