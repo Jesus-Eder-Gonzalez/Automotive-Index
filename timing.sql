2c5c152a83856ab821dd56e7242f0b271ea4475b
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-31 13:48:46 -1000

    Solution to challenge.

--- timing.sql
+++ timing.sql
@@ -1,11 +1,7 @@
--- CREATE INDEX make_idx ON car_models (make_code);
--- CREATE INDEX model_idx ON car_models (model_code);
--- CREATE INDEX year_idx ON car_models (year);
-
 
 SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
 
-SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code= 'GT-R';
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
 
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
 
